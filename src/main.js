6aeb160c948776da41ff2ebba86ae53eb935d46c
Gustavo <Gustavossbbt@gmail.com>
2026-09-01 20:44:24 -0300

    Testando Grid 01/09/26

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@
 import'bootstrap/dist/css/bootstrap.min.css';
 
 // Importando o arquivo de estilo CSS personalizado
-import'./style.css';
+import'/src/style.css';
 
 console.log('Bootstrap e Vite carregados com sucesso');
